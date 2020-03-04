f359e98c5e0f7337af13d6b4dd1966527c8ece15
Christian Sanchez <cas81695@gmail.com>
2020-03-03 20:45:39 -0500

    added methods on game.js and word.js

--- Game/lib/Game.js
+++ Game/lib/Game.js
@@ -4,6 +4,35 @@ const words = require("./words");
 
 // The Game constructor is responsible for keeping score and controlling the flow of the overall game
 class Game {
+    constructor() {
+        this.guessesLeft = 0;
+    }
+
+    play() {
+        this.guessesLeft = 10;
+        this.nextWord();
+    }
+
+    nextWord () {
+
+    }
+
+    makeguess() {
+
+    }
+
+    askToPlayAgain() {
+
+    }
+
+    askForLetter () {
+
+    }
+
+    quit() {
+        console.log("Goodbye!");
+        process.exit(0);
+    }
 
 
 
--- Game/lib/Word.js
+++ Game/lib/Word.js
@@ -2,7 +2,25 @@ const Letter = require("./Letter");
 
 // The Word class is responsible for creating an array of Letter objects and determining if the user guessed every Letter correctly
 class Word {
+    constructor(word) {
+        this.letters = word.split("").map(char => Letter(char));
+    }
 
+    getSolution() {
+        
+    }
+
+    toString() {
+
+    }
+
+    guessLetter() {
+
+    }
+
+    guessedCorrectly() {
+        
+    }
 
 
 
